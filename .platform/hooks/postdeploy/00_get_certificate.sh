a1aaa1d20fea8865f972416bc37d06e90baaefcc
mattyintex <intexmatt@outlook.com>
2024-12-03 18:36:32 -0700

    Update 00_get_certificate.sh

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d 215sc4.is404.net --nginx --agree-tos --email dallenschuetzler@gmail.com
+sudo certbot -n -d 215turtlegroup.is404.net --nginx --agree-tos --email crewjm@byu.edu
